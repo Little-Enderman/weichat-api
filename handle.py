5f59ffdbe10bd01ec0154cbdb3d208b5af8afc31
Little-Enderman <endermanwithhim@icloud.com>
2020-05-05 11:07:52 -0400

    v2 1

--- handle.py
+++ handle.py
@@ -1,31 +1,24 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-# 
 # filename: handle.py
-
 import hashlib
+import reply
+import receive
 import web
-
 class Handle(object):
-    def GET(self):
+    def POST(self):
         try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = "Endermanwithhim1989" #请按照公众平台官网\基本配置中信息填写
-
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
+            webData = web.data()
+            print "Handle Post webdata is ", webData
+            #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
             else:
-                return ""
-        except Exception, Argument:
-            return Argument
+                print "暂且不处理"
+                return "success"
+        except Exception, Argment:
+            return Argment
